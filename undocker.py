c5252072fe377fb0a20ac3298e9ce2dcb14d01b2
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2024-05-30 22:20:44 +0300

    цветной вывод

--- undocker.py
+++ undocker.py
@@ -2,42 +2,68 @@
 import argparse
 import json
 import subprocess
+import sys
+from functools import partial
 from pathlib import Path
 
 DEFAULT_CONFIG_PATH = Path("/etc/docker/daemon.json")
+
+CSI = "\x1b["
+RESET = CSI + "0m"
+RED = CSI + "31m"
+GREEN = CSI + "32m"
+YELLOW = CSI + "33m"
+BLUE = CSI + "34m"
+PURPLE = CSI + "35m"
+
+print = partial(print, file=sys.stderr, flush=True)
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-c", "--config-path", "--config", type=Path, default=DEFAULT_CONFIG_PATH
 )
 
-if __name__ == "__main__":
-    args = parser.parse_args()
+
+def main(argv=None):
+    args = parser.parse_args(argv)
 
     config_path = args.config_path
 
-    if config_path.exists():
-        with config_path.open() as fp:
-            config = json.load(fp)
-    else:
-        config = {}
+    try:
+        if config_path.exists():
+            with config_path.open() as fp:
+                config = json.load(fp)
+        else:
+            config = {}
 
-    config.update(
-        {
-            "registry-mirrors": [
-                # "https://dockerhub.timeweb.cloud",
-                # http://hub-mirror.c.163.com,
-                # https://docker.mirrors.ustc.edu.cn,
-                "https://daocloud.io",
-                "https://c.163.com/",
-                "https://registry.docker-cn.com",
-                "https://mirror.gcr.io",
-                "https://huecker.io",
-            ]
-        }
-    )
+        config.update(
+            {
+                "registry-mirrors": [
+                    # "https://dockerhub.timeweb.cloud",
+                    # "http://hub-mirror.c.163.com",
+                    # "https://docker.mirrors.ustc.edu.cn",
+                    "https://daocloud.io",
+                    "https://c.163.com/",
+                    "https://registry.docker-cn.com",
+                    "https://mirror.gcr.io",
+                    "https://huecker.io",
+                ]
+            }
+        )
 
-    config_path.write_text(
-        json.dumps(config, ensure_ascii=True, indent=2, sort_keys=True)
-    )
+        config_path.write_text(
+            json.dumps(config, ensure_ascii=True, indent=2, sort_keys=True)
+        )
 
-    subprocess.check_call(["systemctl", "restart", "docker"])
+        print(GREEN + "Config succcessfully updated" + CSI)
+
+        subprocess.check_call(["systemctl", "restart", "docker"])
+
+        print(GREEN + "Docker restarted" + CSI)
+    except Exception as ex:
+        print(RED + str(ex) + CSI)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
